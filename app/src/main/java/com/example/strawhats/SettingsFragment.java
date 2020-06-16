4cc4407750facc3d3d2f78c10234e0f3589fa252
mayura gurjar cheepinahalli vasudeva <mayura.cheepinahalli@st.ovgu.de>
2020-06-16 20:02:39 +0200

    help menu

--- app/src/main/java/com/example/strawhats/SettingsFragment.java
+++ app/src/main/java/com/example/strawhats/SettingsFragment.java
@@ -1,21 +1,25 @@
 package com.example.strawhats;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
+Dialog myDialog;
 
 
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_settings,container,false);
@@ -26,6 +30,14 @@ public class SettingsFragment extends Fragment {
                 startActivity(new Intent(getActivity(),currency.class));
             }
         });
+
+
+
+
+
+
        return view;
+
+                
+        }
     }
-}
