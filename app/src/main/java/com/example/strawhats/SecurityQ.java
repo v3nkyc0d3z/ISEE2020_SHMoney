da57c252bc24775a1a818950bc8be5b82a950fd9
Mad1294 <56818426+Mad1294@users.noreply.github.com>
2020-06-20 20:02:24 +0200

    Merge branch 'development' of C:\Users\komal\AndroidStudioProjects\Strawhats\strawhats with conflicts.

---
+++ app/src/main/java/com/example/strawhats/SecurityQ.java
@@ -0,0 +1,14 @@
+package com.example.strawhats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecurityQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_q);
+    }
+}
