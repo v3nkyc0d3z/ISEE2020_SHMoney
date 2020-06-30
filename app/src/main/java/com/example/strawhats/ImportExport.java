0da26864b0b0412c25a0035e5a30ea5b9dda4d38
v3nkyc0d3z <venkatesh.thirugnana@st.ovgu.de>
2020-06-30 20:41:04 +0200

    Import Data

--- app/src/main/java/com/example/strawhats/ImportExport.java
+++ app/src/main/java/com/example/strawhats/ImportExport.java
@@ -5,29 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.core.text.HtmlCompat;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Spannable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class ImportExport extends AppCompatActivity {
     Intent mFileManager;
     TextView filePicker;
+    TransactionDatabaseHelper TDBHelper;
+    String FileLocation;
+    File importFile;
+    Uri importUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_export);
-        TransactionDatabaseHelper TDBHelper = new TransactionDatabaseHelper(this);
+        TDBHelper = new TransactionDatabaseHelper(this);
         Button ExportData = (Button) findViewById(R.id.btnExportData);
         final Cursor Transactions = TDBHelper.getData();
         ExportData.setOnClickListener(new View.OnClickListener() {
@@ -84,19 +98,54 @@ public class ImportExport extends AppCompatActivity {
                 startActivityForResult(mFileManager,1);
             }
         });
+
+        Button importData = (Button) findViewById(R.id.btnImportData);
+        TDBHelper = new TransactionDatabaseHelper(this);
+        importData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<TransactionList> Transactions = new ArrayList<>();
+                String line = "";
+                ContentValues cv;
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(importUri);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+                    while ((line = reader.readLine()) != null){
+                        String[] tokens = line.split(",");
+                        if (!tokens[0].equals("Date")){
+                            Boolean res = TDBHelper.addTransaction(tokens[0],Float.parseFloat(tokens[1]),tokens[2],
+                                                                    tokens[3],tokens[4],tokens[5],tokens[6],"Default");
+                            if (res){
+                                Log.d("line parsed","import successful");
+                            }else{
+                                Log.d("problem","insert failed");
+                            }
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    Log.wtf("Import Failed","No File found"+FileLocation,e);
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Log.wtf("Read Failed","Error reading line found"+line,e);
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK){
-            Uri uri = data.getData();
-            File file = new File(uri.getPath());
-            String name = file.getName();
-            String path = uri.getPath().toString();
-            filePicker.setText(path);
+            importUri = data.getData();
+            importFile = new File(importUri.getPath());
+            String name = importFile.getName();
+            FileLocation = importUri.getPath().toString();
+            filePicker.setText(FileLocation);
         }
     }
+
     private void toastMessage(String message){
         Toast.makeText(this,message,Toast.LENGTH_LONG).show();
     }
